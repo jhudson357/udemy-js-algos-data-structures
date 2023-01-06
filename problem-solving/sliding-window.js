f26e12351ba4e30076b4772d9ccf7bea83cafd02
James Hudson <jmh357@comcast.net>
2023-01-06 17:37:49 -0600

    solve exercise 7 - maxSubarraySum

--- problem-solving/sliding-window.js
+++ problem-solving/sliding-window.js
@@ -47,8 +47,41 @@ function maxSubarraySum2(arr, num){
   return maxSum;
 }
 
-console.log(maxSubarraySum2([1,2,5,2,8,1,5],2)) // 10
-console.log(maxSubarraySum2([1,2,5,2,8,1,5],4)) // 17
-console.log(maxSubarraySum2([4,2,1,6],1)) // 6
-console.log(maxSubarraySum2([4,2,1,6,2],4)) // 13
-console.log(maxSubarraySum2([],4)) // null
+// console.log(maxSubarraySum2([1,2,5,2,8,1,5],2)) // 10
+// console.log(maxSubarraySum2([1,2,5,2,8,1,5],4)) // 17
+// console.log(maxSubarraySum2([4,2,1,6],1)) // 6
+// console.log(maxSubarraySum2([4,2,1,6,2],4)) // 13
+// console.log(maxSubarraySum2([],4)) // null
+
+
+// _____________________________________________
+// _____________________________________________
+
+// EXERCISE 7 (Section 6)
+
+// Given an array of integers and a number, write a function called maxSubarrySum which finds the maximum sum of a subarray with the length of the number passed to the function. 
+// Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+function maxSubarraySum(arr, length) {
+  let maxSum = 0
+  let tempSum = 0
+  if(arr.length < 3) return null
+  for (let i=0; i<length; i++) {
+    maxSum += arr[i]
+    // console.log('maxSum ' ,maxSum)
+  }
+  tempSum = maxSum
+  for(let i=length; i<arr.length; i++) {
+    tempSum = tempSum + arr[i] - arr[i-length]
+    if(tempSum > maxSum) {
+      maxSum = tempSum
+    }
+  }
+  return maxSum
+}
+
+console.log(maxSubarraySum([100,200,300,400], 2)) // 700
+console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4))  // 39 
+console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
+console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
+console.log(maxSubarraySum([2,3], 3)) // null
