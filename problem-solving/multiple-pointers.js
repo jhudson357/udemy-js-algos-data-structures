c2aa6be1c0e138fc09ecc3fa6dd2f3819bc70004
James Hudson <jmh357@comcast.net>
2023-01-06 16:48:27 -0600

    solve exercise 5 - averagePair

--- problem-solving/multiple-pointers.js
+++ problem-solving/multiple-pointers.js
@@ -118,7 +118,38 @@ function areThereDuplicates(...args) {
 }
 
 
-console.log(areThereDuplicates(1, 2, 3)) // false
-console.log(areThereDuplicates(1, 2, 2)) // true 
-console.log(areThereDuplicates('a', 'b', 'c', 'a')) // true 
+// console.log(areThereDuplicates(1, 2, 3)) // false
+// console.log(areThereDuplicates(1, 2, 2)) // true 
+// console.log(areThereDuplicates('a', 'b', 'c', 'a')) // true 
+
+// _____________________________________________
+// _____________________________________________
+
+// EXERISE 5 (Section 6)
+
+// Write a funcion called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+
+
+function averagePair(arr, avg) {
+  // double the avg
+  let double = avg * 2
+  let j = arr.length-1
+
+  if(arr.length <= 1 ) return false
+  // look for a pair that add up to the doubled avg
+  for(let i = 0; i < arr.length; i++) {
+    if((arr[i] + arr[j]) > double) {
+      j --
+    } else if((arr[i] + arr[j] === double)) {
+      return true
+    }
+  }
+  return false
+}
+
+
+console.log(averagePair([1,2,3],2.5)) // true
+console.log(averagePair([1,3,3,5,6,7,10,12,19],8)) // true
+console.log(averagePair([-1,0,3,4,5,6], 4.1)) // false
+console.log(averagePair([],4)) // false
 
