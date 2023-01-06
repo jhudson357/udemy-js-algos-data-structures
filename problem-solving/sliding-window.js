ce13dbab0e57c2e5ec6e93212927ce151272196d
James Hudson <jmh357@comcast.net>
2023-01-06 17:55:08 -0600

    solve exercise 7 - minSubarrayLen

--- problem-solving/sliding-window.js
+++ problem-solving/sliding-window.js
@@ -80,8 +80,50 @@ function maxSubarraySum(arr, length) {
   return maxSum
 }
 
-console.log(maxSubarraySum([100,200,300,400], 2)) // 700
-console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4))  // 39 
-console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
-console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
-console.log(maxSubarraySum([2,3], 3)) // null
+// console.log(maxSubarraySum([100,200,300,400], 2)) // 700
+// console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4))  // 39 
+// console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
+// console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
+// console.log(maxSubarraySum([2,3], 3)) // null
+
+
+// _____________________________________________
+// _____________________________________________
+
+// EXERCISE 7 (Section 6)
+
+// Write a function called minSubarrayLen which accepts two parameters - an array of positive integers and a positive integer.
+// This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+
+function minSubArrayLen(nums, sum) {
+  let total = 0
+  let start = 0
+  let end = 0
+  let minLen = Infinity
+
+  while (start < nums.length) {
+    if(total < sum && end < nums.length) {
+      total += nums[end]
+      end++
+    } 
+    else if(total >= sum) {
+      minLen = Math.min(minLen, end-start)
+      total -= nums[start]
+      start++
+    }
+    else {
+      break
+    }
+  }
+  return minLen === Infinity ? 0 : minLen
+
+}
+
+console.log(minSubArrayLen([2,3,1,2,4,3], 7)) // 2 -> because [4,3] is the smallest subarray
+console.log(minSubArrayLen([2,1,6,5,4], 9)) // 2 -> because [5,4] is the smallest subarray
+console.log(minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52)) // 1 -> because [62] is greater than 52
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],39)) // 3
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],55)) // 5
+console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)) // 2
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],95)) // 0
+
